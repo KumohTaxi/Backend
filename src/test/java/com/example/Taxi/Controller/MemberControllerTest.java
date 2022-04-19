daaa83111814f21de549324146abd7a18d44b08f
star-sil <kimse9811@naver.com>
2022-04-19 19:37:44 +0900

    MemberController 테스트 코드 주석처리

--- src/test/java/com/example/Taxi/Controller/MemberControllerTest.java
+++ src/test/java/com/example/Taxi/Controller/MemberControllerTest.java
@@ -1,40 +1,40 @@
-package com.example.Taxi.Controller;
-
-import com.example.Taxi.controller.MemberController;
-import com.example.Taxi.domain.Member;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = MemberController.class)
-public class MemberControllerTest {
-    @Autowired
-    private MockMvc mvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Test
-    public void 컨트롤러_반환값_테스트() throws Exception{
-        Member member = new Member();
-        member.setId("cialb"); member.setPassword("1234");
-        String content = objectMapper.writeValueAsString(member);
-        mvc.perform(post("/start")
-                        .content(content)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("success"));
-
-    }
-}
+//package com.example.Taxi.Controller;
+//
+//import com.example.Taxi.controller.MemberController;
+//import com.example.Taxi.domain.Member;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(controllers = MemberController.class)
+//public class MemberControllerTest {
+//    @Autowired
+//    private MockMvc mvc;
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Test
+//    public void 컨트롤러_반환값_테스트() throws Exception{
+//        Member member = new Member();
+//        member.setId("cialb"); member.setPassword("1234");
+//        String content = objectMapper.writeValueAsString(member);
+//        mvc.perform(post("/start")
+//                        .content(content)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("success"));
+//
+//    }
+//}
