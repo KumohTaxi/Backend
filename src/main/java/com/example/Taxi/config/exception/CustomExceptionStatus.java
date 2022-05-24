09ca6e911be85b56e04a3a14f2a7e1613d4cc70c
star-sil <kimse9811@naver.com>
2022-05-24 17:17:37 +0900

    FIX 로그인 시 유효하지 않은 인가코드에 대한 예외처리 추가 및 로그 추가

--- src/main/java/com/example/Taxi/config/exception/CustomExceptionStatus.java
+++ src/main/java/com/example/Taxi/config/exception/CustomExceptionStatus.java
@@ -12,7 +12,8 @@ public enum CustomExceptionStatus {
     NOT_EXIST_GROUP(1002,"속한 그룹이 존재하지 않습니다."),
     INVALID_TOKEN(1003,"유효하지 않은 토큰입니다."),
     NOT_EQUAL_GROUP_GENDER(1004,"그룹 성별과 사용자의 성별이 일치하지 않습니다."),
-    EXPIRE_SESSION(1005,"세션이 만료되었습니다.");
+    EXPIRE_SESSION(1005,"세션이 만료되었습니다."),
+    INVALID_AUTH_CODE(1006,"유효하지 않은 인가코드입니다.");
 
     private final int errorCode;
     private final String message;
