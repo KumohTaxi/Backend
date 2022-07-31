13d03ad23d1a66b15fc0f4ab8d8dce745d2a0916
star-sil <kimse9811@naver.com>
2022-07-31 19:50:31 +0900

    UPDATE createGroup메서드 수정 및 throws Exception 제거
    가독성을 높이기위해 코드를 수정했다.

--- src/main/java/com/example/Taxi/group/GroupController.java
+++ src/main/java/com/example/Taxi/group/GroupController.java
@@ -29,14 +29,10 @@ public class GroupController {
     private final MemberService memberService;
 
     @PostMapping("/group/new")
-    public void createGroup(@RequestBody @Valid GroupRequestDto groupReqDto) throws Exception {
+    public void createGroup(@RequestBody @Valid GroupRequestDto groupReqDto) {
         Member member = memberService.findMemberByAccessToken(groupReqDto.getAccessToken());
-        if (member.getGroup() == null) {
-            groupService.createGroup(groupReqDto, member);
-            log.info("방 생성 성공!!" + member.getGroup().getDestination());
-        } else {
-            throw new CustomException(CustomExceptionStatus.IMPOSSIBLE_ENTER_GROUP);
-        }
+        if (member.getGroup() == null) groupService.createGroup(groupReqDto, member);
+        else throw new CustomException(CustomExceptionStatus.IMPOSSIBLE_ENTER_GROUP);
     }
 
     @PostMapping("/group")
@@ -50,28 +46,28 @@ public class GroupController {
     }
 
     @GetMapping("/group/member/{accessToken}")
-    public GroupResponseDto findMyGroup(@PathVariable String accessToken) throws Exception {
+    public GroupResponseDto findMyGroup(@PathVariable String accessToken) {
         return groupService.findMyGroup(accessToken);
     }
 
     @PostMapping("/group/{id}")
-    public Long enterGroup(@PathVariable Long id, @RequestBody TokenDto tokenDto) throws Exception {
+    public Long enterGroup(@PathVariable Long id, @RequestBody TokenDto tokenDto) {
         log.info(tokenDto.getAccessToken());
         return groupService.enter(id, tokenDto.getAccessToken());
     }
 
     @GetMapping("/group/{id}")
-    public GroupResponseDto findGroupById(@PathVariable Long id) throws Exception {
+    public GroupResponseDto findGroupById(@PathVariable Long id) {
         return new GroupResponseDto(groupService.findOne(id));
     }
 
     @PostMapping("/group/{id}/exit")
-    public void exitGroup(@PathVariable Long id, @RequestBody TokenDto tokenDto) throws Exception{
+    public void exitGroup(@PathVariable Long id, @RequestBody TokenDto tokenDto) {
         groupService.exit(id, tokenDto.getAccessToken());
     }
 
     @GetMapping("/group/{id}/post")
-    public List<PostResDto> getPost(@PathVariable Long id) throws Exception {
+    public List<PostResDto> getPost(@PathVariable Long id) {
         List<PostResDto> postResDtos = new ArrayList<>();
         for(Post post : groupService.findOne(id).getPosts()){
             postResDtos.add(new PostResDto(post));
