c73c388732bb12dcda22245e6d9bacf24a4d2b80
star-sil <kimse9811@naver.com>
2022-10-10 07:55:52 +0900

    FEAT S3 이미지 업로드 기능 추가
    --

---
+++ src/main/java/com/example/Taxi/image/AmazonS3Component.java
@@ -0,0 +1,15 @@
+package com.example.Taxi.image;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class AmazonS3Component {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+    @Value("${cloud.aws.s3.folder}")
+    private String folderName;
+}
